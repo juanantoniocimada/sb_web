84be85941b50f37966864603fdaf5d777ba0a67a
JUAN ANTONIO DURÁN <142181516+juanantoniocimada@users.noreply.github.com>
2025-12-22 10:44:01 +0000

    feat: rutas que funcionan

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -24,14 +24,9 @@ export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage),
-    
   },
   {
     path: 'links',
     loadComponent: () => import('./pages/links/links.page').then((m) => m.LinksPage),
-  },
-  {
-    path: ':lang/:origin/:destination',
-    loadComponent: () => import('./pages/lines/lines.page').then((m) => m.LinesPage),
-  },
+  }
 ];
