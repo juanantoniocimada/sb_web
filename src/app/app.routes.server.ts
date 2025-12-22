9dd90ca2ea9669b428a3c1529a9c1336b1f2dd1e
JUAN ANTONIO DURÁN <142181516+juanantoniocimada@users.noreply.github.com>
2025-12-22 10:55:04 +0000

    feat: server routes antiguo

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,16 +2,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '',
-    renderMode: RenderMode.Prerender,
-  },
-  {
-    path: 'links',
-    renderMode: RenderMode.Prerender,
-  },
-  {
-    path: ':lang/:origin/:destination',
-    // Al no especificar 'renderMode', se usará SSR dinámico por defecto.
-    // Esto evita el error de pre-renderizado en rutas dinámicas.
-  },
+    path: '**',
+    renderMode: RenderMode.Prerender
+  }
 ];
